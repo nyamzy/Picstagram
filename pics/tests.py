f1aa06e11a71f828543afe2316c265213168e8ef
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 21:03:57 +0300

    create test to save location

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.home = Location(place = "Home")
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.home, Location))
+
+    # Testing the save method
+    def test_save_method(self):
+        self.home.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.home.save_location()
+        self.home.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) < 1)
