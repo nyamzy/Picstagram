f0443abc841b5b11a5b2f05fd04ffb622475ca33
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 21:33:59 +0300

    create test for updating location

--- pics/tests.py
+++ pics/tests.py
@@ -17,8 +17,46 @@ class LocationTestClass(TestCase):
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
 
+    # Testing the delete method
     def test_delete_method(self):
         self.home.save_location()
         self.home.delete_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations) < 1)
+
+    # Testing the update method
+    def test_update_method(self):
+        self.home.save_location()
+        self.home.update_location()
+        updated_location = Location.objects.filter(place = "Home").update(place = "Away")
+        self.assertTrue(self.home.place != updated_location)
+
+
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.travel = Category(category = "Travel")
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel, Category))
+
+    # # Testing the save method
+    # def test_save_method(self):
+    #     self.home.save_location()
+    #     locations = Location.objects.all()
+    #     self.assertTrue(len(locations) > 0)
+
+    # # Testing the delete method
+    # def test_delete_method(self):
+    #     self.home.save_location()
+    #     self.home.delete_location()
+    #     locations = Location.objects.all()
+    #     self.assertTrue(len(locations) < 1)
+
+    # # Testing the update method
+    # def test_update_method(self):
+    #     self.home.save_location()
+    #     self.home.update_location()
+    #     updated_location = Location.objects.filter(place = "Home").update(place = "Away")
+    #     self.assertTrue(self.home.place != updated_location)
