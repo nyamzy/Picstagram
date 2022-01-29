63291a9c65dc3f3ca18eb2ec97d36722f63104d9
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 21:47:10 +0300

    create test for saving category method

--- pics/tests.py
+++ pics/tests.py
@@ -30,6 +30,7 @@ class LocationTestClass(TestCase):
         self.home.update_location()
         updated_location = Location.objects.filter(place = "Home").update(place = "Away")
         self.assertTrue(self.home.place != updated_location)
+        self.assertNotEqual(self.home, updated_location)
 
 
 class CategoryTestClass(TestCase):
@@ -41,11 +42,11 @@ class CategoryTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.travel, Category))
 
-    # # Testing the save method
-    # def test_save_method(self):
-    #     self.home.save_location()
-    #     locations = Location.objects.all()
-    #     self.assertTrue(len(locations) > 0)
+    # Testing the save method
+    def test_save_method(self):
+        self.travel.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
 
     # # Testing the delete method
     # def test_delete_method(self):
