ca4827a85e9877b1e4095b464d1ed8b80b4ca7c0
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 21:03:21 +0300

    create method to save location

--- pics/models.py
+++ pics/models.py
@@ -4,16 +4,29 @@ from django.db import models
 class Location(models.Model):
     place = models.CharField(max_length = 30)
 
+    def __str__(self):
+        return self.place
+    
+    # Save location method
+    def save_location(self):
+        self.save()
+
 
 class Category(models.Model):
     category = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.category
 
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 100)
-    image_location = models.ForeignKey(Location)
-    image_category = models.ForeignKey(Category)
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     post_date = models.DateTimeField(auto_now_add = True)
     url = models.URLField(max_length = 200)
+
+    def __str__(self):
+        return self.image_name
