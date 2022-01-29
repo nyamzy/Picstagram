bcea31d8e7337e28cd342fd1626aff657df01bff
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 12:17:48 +0300

    configure urls to register the media root route

--- pics/urls.py
+++ pics/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', )
+    path('', views.welcome, name = "welcome")
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
