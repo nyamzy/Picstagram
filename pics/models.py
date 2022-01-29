f319a3a8e0c0c873ddb72f4c4420c2aa80c0bbba
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 21:05:35 +0300

    create method to delete location

--- pics/models.py
+++ pics/models.py
@@ -11,6 +11,10 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
+    # Delete location method
+    def delete_location(self):
+        self.delete()
+
 
 class Category(models.Model):
     category = models.CharField(max_length = 20)
