2c502fbcc048cf579a1e7e6b7a3a7bfa7cd2274d
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-29 13:34:08 +0300

    create the location model

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    place = models.CharField(max_length = 30)
+
+
+class Category(models.Model):
+    pass
+    # category = models.CharField(max_length = 20)
+
+
+class Image(models.Model):
+    pass
+    # image = models.ImageField(upload_to = 'images/')
+    # image_name = models.CharField(max_length = 30)
+    # description = models.CharField(max_length = 100)
+    # image_location = models.ForeignKey(Location)
+    # image_category = models.ForeignKey(Category)
+    # post_date = models.DateTimeField(auto_now_add = True)
+    # url = models.URLField(max_length = 200)
