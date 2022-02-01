d17b8ad776e16d1e7b88a89dc28f4ac5b8acd113
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-02 00:05:01 +0300

    create search results view function for searching by category

--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Image
 
@@ -10,3 +11,18 @@ def welcome(request):
         "image_list": image_list
     }
     return render(request, "welcome.html", context)
+
+
+def search_results(request):
+    try:
+        if 'picture' in request.GET and request.GET["picture"]:
+            search_term = request.GET.get("picture")
+            searched_images = Image.search_by_category(search_term)
+            message = f'{search_term}'
+            return render(request, "search_results.html", {"message": message, "pictures": searched_images})
+        else:
+            message = "You haven't searched for any term"
+            return render(request, "search_results.html", {"message": message})
+    except Image.DoesNotExist:
+        raise Http404
+    
