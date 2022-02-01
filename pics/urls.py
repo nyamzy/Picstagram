d8ed309f804f2dea2b72a1949cd9ba77c6e8b1dc
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-01 22:38:07 +0300

    create url for searching by category

--- pics/urls.py
+++ pics/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.welcome, name = "welcome")
+    path('', views.welcome, name = "welcome"),
+    path('search/', views.search_results, name="search_results"),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
